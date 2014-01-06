71425c37828c61ba4fa6beca0e935845734ad72d
kaymorey <moreirakatia@neuf.fr>
2014-01-06 20:04:32 +0100

    added initial structure and dependencies

---
+++ Server/lib/social-app.js
@@ -0,0 +1,13 @@
+/*
+ * social-app
+ * https://github.com/kaymorey/social-app
+ *
+ * Copyright (c) 2014 Katia Moreira
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+exports.awesome = function() {
+  return 'awesome';
+};
